de40e617b27cb302cdbaab9b192d0320d426163a
Harun Kološ <hkolos1@etf.unsa.ba>
2022-05-24 09:32:11 +0200

    CharacterController and Models

---
+++ Mistral-Internship/Controllers/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mistral_Internship.Models;
+
+namespace Mistral_Internship.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get()
+        {
+            return Ok(knight);
+        }
+
+    }
+}
---
+++ Mistral-Internship/Models/RpgClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Mistral_Internship.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight,
+        Mage,
+        Cleric,
+
+    }
+}
