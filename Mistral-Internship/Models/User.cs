e2cd3fd06bc2971fd28522cecf77fc7dafc8f1e9
Harun Kološ <hkolos1@etf.unsa.ba>
2022-05-25 15:48:01 +0200

    First Relationship

---
+++ Mistral-Internship/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Mistral_Internship.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
