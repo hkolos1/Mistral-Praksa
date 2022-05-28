48763b6a0eef1367ec99a585a2198e4f505f698f
Harun Kološ <hkolos1@etf.unsa.ba>
2022-05-28 22:20:04 +0200

    Start a Fight!

---
+++ Mistral-Internship/Dtos/Fight/FightResultDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Mistral_Internship.Dtos.Fight
+{
+    public class FightResultDto
+    {
+        public List<string> Log { get; set; } = new List<string>();
+    }
+}
--- Mistral-Internship/Models/Character.cs
+++ Mistral-Internship/Models/Character.cs
@@ -4,7 +4,7 @@ namespace Mistral_Internship.Models
 {
     public class Character
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = "Frodo";
 
         public int HitPoints { get; set; } = 100;
